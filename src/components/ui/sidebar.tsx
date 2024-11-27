82f3a8f60c1b19a9e85abaf644d89e70795d0631
M4rshe1 <colin.heggli@gmail.com>
2024-11-27 15:57:56 +0100

    removed unused import

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -13,7 +13,6 @@ import {Separator} from "@/components/ui/separator"
 import {Sheet, SheetContent} from "@/components/ui/sheet"
 import {Skeleton} from "@/components/ui/skeleton"
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger,} from "@/components/ui/tooltip"
-import {Session} from "next-auth";
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
